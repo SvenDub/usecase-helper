955a5b27a7aa17c3aa2d0f25bfe5690fc8e44a2e
Sven Dubbeld <s.dubbeld@student.fontys.nl>
2016-02-28 10:37:09 +0100

    Show status while hovering over canvas

--- UsecaseHelper/MainForm.cs
+++ UsecaseHelper/MainForm.cs
@@ -189,6 +189,8 @@ namespace UsecaseHelper
         private void imgDrawing_MouseLeave(object sender, System.EventArgs e)
         {
             SelectedDrawable = null;
+
+            statusBarLabel.Text = "Ready";
         }
 
         private void imgDrawing_MouseMove(object sender, MouseEventArgs e)
@@ -198,6 +200,64 @@ namespace UsecaseHelper
                 SelectedDrawable.GhostX = e.X;
                 SelectedDrawable.GhostY = e.Y;
                 imgDrawing.Invalidate();
+            }
+
+            if (rdiModeCreate.Checked)
+            {
+                if (rdiElementActor.Checked)
+                {
+                    statusBarLabel.Text = "Create a new actor";
+                }
+                else if (rdiElementUseCase.Checked)
+                {
+                    statusBarLabel.Text = "Create a new use case";
+                }
+                else if (rdiElementLine.Checked)
+                {
+                    if (_selectedActor == null)
+                    {
+                        Drawable selection = _drawables.Find(drawable => drawable.InSelection(e.X, e.Y) && drawable is Actor);
+                        statusBarLabel.Text = selection == null ? "Select an actor" : $"Link {selection.Name}";
+                    }
+                    else
+                    {
+                        Drawable selection = _drawables.Find(drawable => drawable.InSelection(e.X, e.Y) && drawable is UseCase);
+                        statusBarLabel.Text = selection == null ? "Select a use case" : $"Link {_selectedActor.Name} to {selection.Name}";
+                    }
+                }
+            }
+            else if (rdiModeDelete.Checked)
+            {
+                Drawable selection = _drawables.Find(drawable => drawable.InSelection(e.X, e.Y));
+
+                statusBarLabel.Text = selection != null ? $"Delete {selection.Name}" : "Ready";
+            }
+            else if (rdiModeUnlink.Checked)
+            {
+                if (_selectedActor == null)
+                {
+                    Drawable selection = _drawables.Find(drawable => drawable.InSelection(e.X, e.Y) && drawable is Actor);
+                    statusBarLabel.Text = selection == null ? "Select an actor" : $"Unlink {selection.Name}";
+                }
+                else
+                {
+                    Drawable selection = _drawables.Find(drawable => drawable.InSelection(e.X, e.Y) && drawable is UseCase);
+                    statusBarLabel.Text = selection == null ? "Select a use case" : $"Unlink {_selectedActor.Name} from {selection.Name}";
+                }
+            } 
+            else if (rdiModeSelect.Checked)
+            {
+                if (SelectedDrawable != null)
+                {
+                    statusBarLabel.Text = $"Moving {SelectedDrawable.Name}";
+                }
+                else
+                {
+                    Drawable selection = _drawables.Find(drawable => drawable.InSelection(e.X, e.Y));
+                    statusBarLabel.Text = selection == null
+                        ? "Ready"
+                        : $"Click to select {selection.Name} - Drag to move";
+                }
             }
         }
 
